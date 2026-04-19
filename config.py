aa2afe8afdc3da5b27339c8993e8a5d3d2b0d512
Thory <148980437+thory99@users.noreply.github.com>
2026-04-19 10:31:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,9 +25,53 @@ SITE_CONFIG = {
     "default_region": "IND",
     
     # Footer
-    "footer_text": "THORYXFF • Neon Bio Injector",   # ← comma lagao!
+    "footer_text": "THORYXFF • Neon Bio Injector",   # ← comma is correct here!
     
-    # Video Tutorial Links (NEW)
+    # Video Tutorial Links
     "howto_video_link": "https://www.youtube.com/@GamesOfThory",
-    "create_own_video_link": "https://www.youtube.com/@GamesOfThory"   # ← last line, no comma
+    "create_own_video_link": "https://www.youtube.com/@GamesOfThory",   # ← last line, no comma
+
+
+    "templates": [
+        {
+            "name": "THORY SPECIAL",
+            "bio": "[B][C][I][FEA100]ㅤ ╭─⎾ Thory ⏋[FFFFFF] ↓[FF1493]IG THORYXFF[FFFFFF]↓\n ╭┘▣│ᴀɪ ᴡᴏʀʟᴅ│[78CDFF]YT - GAMESOFTHORY\n[7AC279][U]└◎─┴─◎─◎[/U]┘[F5BE63]EMOTE BOT KE LIYE YT CHECK"
+        },
+        {
+            "name": "SHER - E - FF",
+            "bio": "[B][C][I][FF0000]Dil mein aag, aankhon mein toofan\n[FF6600]Khel ke dikha, mujhse bana jo baat\n[FFD700]Mera rank teri soch se bhi upar"
+        },
+        {
+            "name": "KALAKAR",
+            "bio": "[B][C][I][8B00FF]Main gamer nahi, ek kalakar hoon\n[9370DB]Headshot mera jaadu, hai yeh kamaal\n[7B68EE]Tujhe dikh jaayega jab hoga mera saamna"
+        },
+        {
+            "name": "BADSHAH",
+            "bio": "[B][C][I][FF1493]Jo bich me aaya, uski lag gayi\n[FF69B4]Mera game hai teri neend ka dushman\n[DB7093]Badshah hoon main, yeh hai mera sheher"
+        },
+        {
+            "name": "TOOFAN",
+            "bio": "[B][C][I][1E90FF]Aaya hoon toofan lekar, rukna nahi\n[00BFFF]Ek headshot, ek kill, mera hai yeh hunar\n[87CEEB]Pichhe hat ja, warna ho jaayega sabak"
+        },
+        {
+            "name": "ANDAZ",
+            "bio": "[B][C][I][FFD700]Andaz mera hai kuch khaas\n[FFA500]Har game mein lagta hai mera raaj\n[FF8C00]Tujhe dikh jaayega mera andaaz-e-khas"
+        },
+        {
+            "name": "GAME OVER",
+            "bio": "[B][C][I][FF4500]Tu khelta hai, main khel khatam karun\n[FF6347]Shuru se pehle hi tera game over\n[FF7F50]Wapas jaake practice kar, bachke reh"
+        },
+        {
+            "name": "MASTANA",
+            "bio": "[B][C][I][9400D3]Game khelta hoon mastana andaz se\n[BA55D3]Dushman ki aankh mein goli hai raat din\n[DA70D6]Rone ki naubat aayegi jab hoga saamna"
+        },
+        {
+            "name": "FAUJI",
+            "bio": "[B][C][I][228B22]Fauji hoon main, game hai mera yudh\n[32CD32]Ek trigger, ek nishana, hai mera wada\n[7CFC00]Pichhe hat ja warna ho jaayega tabah"
+        },
+        {
+            "name": "RAJA",
+            "bio": "[B][C][I][B22222]Raja hoon main game ka, hai yeh taaj\n[DC143C]Jo mere saamne aaya, woh hua barbaad\n[FF6347]Tera kya kaam yahan, ab ja bach ke"
+        }
+    ]
 }
